3d1a9a673207c2da2d4462beef6e4836bbfd3b0f
sunnepazzy123 <sunnepazzy123@gmail.com>
2021-04-27 15:05:22 +0200

    my first vuetify project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav>
+     <v-app-bar color="deep-purple accent-4" dense dark >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Courier Service</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn color="red">
+        <span>Login</span>
+        <v-icon right>mdi-login</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+       </v-app-bar>
+
+        <v-navigation-drawer app v-model="drawer">
+        <v-list nav dense >
+        <v-list-item
+          active-class="deep-purple--text text--accent-4"
+           v-for="item in items"
+            :key="item.title"
+             link
+        >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+          <v-list-item>
+            <v-list-item-content>{{ item.title }}</v-list-item-content>
+          </v-list-item>
+        </v-list-item>
+      </v-list>
+
+      </v-navigation-drawer>
+
+  </nav>
+</template>
+
+<script>
+export default {
+
+  data(){
+    return {
+      drawer: false,
+      items: [
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'About', icon: 'mdi-forum' },
+        ],
+
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Courier Service</h1>
+    <v-btn class="indigo white--text "   elevation="2"> Click me</v-btn>
+    <v-btn color="pink" class="ma-2"> Click me</v-btn>
+    <v-btn color="red" depressed>
+      email
+      <v-icon>
+          mdi-checkbox-marked-circle</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
